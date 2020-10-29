678f712e2ba88dccb0ba864414049c260df13789
saladking <1624573567@qq.com>
2020-10-29 18:00:08 +0800

    finish

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,5 +1,7 @@
+import static org.junit.Assert.*;
+
 public class HorribleSteve {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
