7e028f2d564357055e1db524c96cc0354dce9686
saladking <1624573567@qq.com>
2020-12-15 22:19:20 +0800

    finish

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,13 +1,17 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    ArrayDeque<Integer> fringe = new ArrayDeque<>();
+
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -18,20 +22,64 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target. */
+    /**
+     * Estimate of the distance from v to the target.
+     */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(t) - maze.toX(v)) + Math.abs(maze.toY(t) - maze.toY(v));
     }
 
-    /** Finds vertex estimated to be closest to target. */
+    /**
+     * Finds vertex estimated to be closest to target.
+     */
     private int findMinimumUnmarked() {
-        return -1;
+        int min = Integer.MAX_VALUE;
+        int res = fringe.peek();
+        for (int w : fringe) {
+            if (h(w) < min) {
+                min = h(w);
+                res = w;
+            }
+        }
+        return res;
         /* You do not have to use this method. */
     }
 
-    /** Performs an A star search from vertex s. */
+    /**
+     * Performs an A star search from vertex s.
+     */
     private void astar(int s) {
         // TODO
+
+
+        fringe.add(s);
+        marked[s] = true;
+
+        while (!fringe.isEmpty()) {
+
+            int v = findMinimumUnmarked();
+            marked[v] = true;
+            fringe.remove();
+            announce();
+
+            if (v == t) {
+                targetFound = true;
+            }
+
+            if (targetFound) {
+                return;
+            }
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    fringe.add(w);
+
+                }
+            }
+        }
     }
 
     @Override
