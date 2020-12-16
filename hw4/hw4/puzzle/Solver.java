733b96fb9750416c1eefb666f237eeb2b95a1ef2
saladking <1624573567@qq.com>
2020-12-16 23:28:08 +0800

    Solver finished

---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,95 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class Solver {
+
+    private SearchNode head;
+    private SearchNode tail;
+
+    public class SearchNode implements Comparable<SearchNode> {
+
+        //a WorldState.
+        private WorldState state;
+        //the number of moves made to reach this world state from the initial state.
+        private int numOfMoves = 0;
+        //a reference to the previous search node.
+        private SearchNode prevSearchNode;
+
+        public SearchNode(WorldState state, int numOfMoves, SearchNode prevSearchNode) {
+            this.state = state;
+            this.numOfMoves = numOfMoves;
+            this.prevSearchNode = prevSearchNode;
+        }
+
+        @Override
+        public int compareTo(SearchNode other) {
+            return (this.numOfMoves + this.state.estimatedDistanceToGoal())
+                    - (other.numOfMoves + other.state.estimatedDistanceToGoal());
+        }
+
+    }
+
+    /**
+     * Constructor which solves the puzzle, computing
+     * everything necessary for moves() and solution() to
+     * not have to solve the problem again. Solves the
+     * puzzle using the A* algorithm. Assumes a solution exists.
+     */
+    public Solver(WorldState initial) {
+        MinPQ<SearchNode> pq = new MinPQ<>();
+        this.head = new SearchNode(initial, 0, null);
+        pq.insert(this.head);
+
+        /**
+         * Remove the search node with minimum priority. Let’s call this node X.
+         * If it is the goal node, then we’re done.
+         * Otherwise, for each neighbor of X’s world state, create a new search node
+         * that obeys the description above and insert it into the priority queue.
+         */
+        while (!pq.isEmpty()) {
+
+            SearchNode currentNode = pq.delMin();
+            if (currentNode.state.isGoal()) {
+                this.tail = currentNode;
+                break;
+            }
+
+            for (WorldState neighbor : currentNode.state.neighbors()) {
+
+                // A critical optimization checks that no enqueued WorldState is its own
+                if (currentNode.prevSearchNode != null && neighbor.equals(currentNode.prevSearchNode.state)) {
+                    continue;
+                }
+
+                SearchNode newSearchNode = new SearchNode(neighbor, currentNode.numOfMoves + 1, currentNode);
+                pq.insert(newSearchNode);
+            }
+        }
+    }
+
+    /**
+     * Returns the minimum number of moves to solve the puzzle
+     * starting at the initial WorldState.
+     */
+    public int moves() {
+        return this.tail.numOfMoves;
+    }
+
+    /**
+     * Returns a sequence of WorldStates from the initial WorldState to the solution.
+     */
+    public Iterable<WorldState> solution() {
+        ArrayDeque<WorldState> ans = new ArrayDeque<>();
+        do {
+            ans.push(this.tail.state);
+            this.tail = this.tail.prevSearchNode;
+        } while (this.tail != this.head);
+        return ans;
+    }
+
+}
