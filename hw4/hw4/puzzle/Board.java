3ba4d7f26987d3c3f1a6daa54e45cd11620abd29
saladking <1624573567@qq.com>
2020-12-18 08:56:43 +0800

    bugs unfixed

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -20,7 +20,7 @@ public class Board implements WorldState {
 
         for (int i = 0; i <= N - 1; i++) {
             for (int j = 0; j <= N - 1; j++) {
-                this.tiles[i][j] = tileAt(i, j);
+                this.tiles[i][j] = tiles[i][j];
             }
         }
 
